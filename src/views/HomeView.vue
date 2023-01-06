f77c88727b3d61eba212455ecb8586f362556b92
LK <laszlo.kovary@gmail.com>
2023-01-07 00:03:40 +0100

    Adding prj name to the displayed mess.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to the Technical Quick References Vue.js App"/>
   </div>
 </template>
 
