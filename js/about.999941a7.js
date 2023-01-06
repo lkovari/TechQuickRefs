e166309807463f69660fa1e41adc1bb2b210be82
LK <laszlo.kovary@gmail.com>
2023-01-07 00:08:53 +0100

    adding dist

--- js/about.999941a7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunktech_quick_refs"]=self["webpackChunktech_quick_refs"]||[]).push([[443],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return o}});var t=u(3396);const c={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function a(e,n){return(0,t.wg)(),(0,t.iD)("div",c,s)}var i=u(89);const f={},h=(0,i.Z)(f,[["render",a]]);var o=h}}]);
-//# sourceMappingURL=about.999941a7.js.map
